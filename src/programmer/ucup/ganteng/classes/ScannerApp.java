6a019736f73f35fec87c1e2824b1f526a32d0830
ynw99 <yusufedu99@gmail.com>
2021-11-11 09:53:36 +0700

    scanner class

---
+++ src/programmer/ucup/ganteng/classes/ScannerApp.java
@@ -0,0 +1,19 @@
+package programmer.ucup.ganteng.classes;
+
+import java.util.Scanner;
+
+public class ScannerApp {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Nama: ");
+    String name = scanner.nextLine();
+
+    System.out.print("Umur: ");
+    Integer age = scanner.nextInt();
+
+    System.out.println("Hello " + name + " umurmu adalah " + age + ".");
+  }
+}
