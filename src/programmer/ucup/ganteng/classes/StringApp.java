b3b88bedd7f79945bb5bf92e394a2ab75ba2ca49
ynw99 <yusufedu99@gmail.com>
2021-11-11 08:56:08 +0700

    string class

--- src/programmer/ucup/ganteng/classes/StringApp.java
+++ src/programmer/ucup/ganteng/classes/StringApp.java
@@ -1,4 +1,33 @@
 package programmer.ucup.ganteng.classes;
 
 public class StringApp {
+
+  public static void main(String[] args) {
+
+    String name = "Yusuf Nur Wahid";
+    String nameLowercase = name.toLowerCase();
+    String nameUppercase = name.toUpperCase();
+
+    System.out.println(name);
+    System.out.println(nameLowercase);
+    System.out.println(nameUppercase);
+    System.out.println(name.length());
+    System.out.println(name.startsWith("Yus"));
+    System.out.println(name.endsWith("hid"));
+
+    String[] names = name.split(" ");
+    for (var value : names) {
+      System.out.println("Name value: " + value);
+    }
+
+    System.out.println(" ".isBlank());
+    System.out.println(" ".isEmpty());
+    System.out.println("".isEmpty());
+    System.out.println(name.charAt(0));
+
+    char[] chars = name.toCharArray();
+    for (var ch : chars) {
+      System.out.println("Char array: " + ch);
+    }
+  }
 }
