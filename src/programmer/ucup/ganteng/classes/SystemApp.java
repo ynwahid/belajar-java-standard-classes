6a17fee5f9ef4e5b5fbc7631ce520b1421da14c3
ynw99 <yusufedu99@gmail.com>
2021-11-11 14:11:36 +0700

    system class

---
+++ src/programmer/ucup/ganteng/classes/SystemApp.java
@@ -0,0 +1,18 @@
+package programmer.ucup.ganteng.classes;
+
+public class SystemApp {
+
+  public static void main(String[] args) {
+
+    System.out.println(System.currentTimeMillis());
+    System.out.println(System.nanoTime());
+
+    System.out.println(System.getenv("HOME"));
+    System.out.println(System.getenv("APP"));
+
+    System.gc();
+    System.exit(1);
+
+    System.out.println("Hi");
+  }
+}
